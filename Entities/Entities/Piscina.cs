2c4789926db1185745a7f7785058bf4508c0cee2
Warias30360 <wilsonariaslopez.tf@gmail.com>
2024-04-07 02:18:00 -0600

    Se cambia el string de conexion para uso local de Wilson Se agrega la clase Piscina

---
+++ Entities/Entities/Piscina.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Entities;
+
+public partial class Piscina
+{
+    public int Id { get; set; }
+    public int ClienteId { get; set; }
+    public Cliente Cliente { get; set; }
+    public int? ServicioId { get; set; }
+    public Servicio Servicio { get; set; }
+    public string Nombre { get; set; }
+    public byte[] Imagen { get; set; }
+    public string Comentario { get; set; }
+    public string EmpleadoComentario { get; set; }
+    public DateTime? FechaHoraComentario { get; set; }
+}
